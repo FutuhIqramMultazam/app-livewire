363586eef4ace1690a5bb76ba133311cee2a5074
FutuhIqramMultazam <futuhiqramm@gmail.com>
2024-11-21 02:16:18 +0700

    sukses menginstal livewire

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,9 @@
+<div>
+    <h1>{{ $count }}</h1>
+ 
+    <br>
+
+    <button class="p-2 mr-2 rounded-full bg-green-700" wire:click="increment">+</button>
+ 
+    <button class="p-2 bg-red-700 rounded-full" wire:click="decrement">-</button>
+</div>
